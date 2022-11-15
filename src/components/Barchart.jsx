f11eea87cf88e089bcdc27cef9f75528a450b459
makaracc <makara.chhaing@gmail.com>
2022-11-15 17:52:08 +1100

    add state

--- src/components/Barchart.jsx
+++ src/components/Barchart.jsx
@@ -5,23 +5,23 @@ import { API } from "../api/api";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-export const data = {
-  labels: ["Red", "Blue", "Yellow", "Green"],
-  datasets: [
-    {
-      label: "Top 4 Baby Names",
-      data: [12, 19, 3, 5],
-      backgroundColor: ["#9A1663", "#FF8787", "#BCEAD5", "#8D72E1"],
-      borderColor: ["#9A1663", "#FF8787", "#BCEAD5", "#8D72E1"],
-      borderWidth: 1,
-    },
-  ],
-};
-
 export const Barchart = () => {
   const [babies, setBabies] = useState(
     JSON.parse(window.sessionStorage.getItem("top100"))
   );
+
+  const [data, setData] = useState({
+    labels: ["Red", "Blue", "Yellow", "Green"],
+    datasets: [
+      {
+        label: "Top 4 Baby Names",
+        data: [12, 19, 3, 5],
+        backgroundColor: ["#9A1663", "#FF8787", "#BCEAD5", "#8D72E1"],
+        borderColor: ["#9A1663", "#FF8787", "#BCEAD5", "#8D72E1"],
+        borderWidth: 1,
+      },
+    ],
+  });
 
   const [query, setQuery] = useState(false);
 
@@ -36,13 +36,35 @@ export const Barchart = () => {
         .filter((baby) => baby.year === 2020 && baby.sex === "MALE")
         .map((baby) => baby.name)
     );
-    data.datasets[0].data = babies
-      .filter((baby) => baby.year === 2020 && baby.sex === "MALE")
-      .map((baby) => baby.count);
-    data.labels = babies
-      .filter((baby) => baby.year === 2020 && baby.sex === "MALE")
-      .map((baby) => baby.name);
+    setData((preData) => ({
+      ...preData,
+      labels: babies
+        .filter((baby) => baby.year === 2020 && baby.sex === "MALE")
+        .map((baby) => baby.name),
+    }));
+    setData((preData) => ({
+      ...preData,
+      datasets: [
+        {
+          label: "Top 4 Baby Names",
+          data: babies
+            .filter((baby) => baby.year === 2020 && baby.sex === "MALE")
+            .map((baby) => baby.count),
+          backgroundColor: ["#9A1663", "#FF8787", "#BCEAD5", "#8D72E1"],
+          borderColor: ["#9A1663", "#FF8787", "#BCEAD5", "#8D72E1"],
+          borderWidth: 1,
+        },
+      ],
+    }));
+    // setData(
+    //   (data) =>
+    //     (data.labels = babies
+    //       .filter((baby) => baby.year === 2020 && baby.sex === "MALE")
+    //       .map((baby) => baby.name))
+    // );
   }, [query, babies]);
+
+  console.log("data", data.labels);
 
   return <Pie data={data} />;
 };
